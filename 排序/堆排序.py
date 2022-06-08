cc55847ca67a57fa00771cf314b3fa5b2f007924
aiml-kiki <1106825916@qq.com>
2022-06-08 20:31:33 +0800

    没有改动

--- 排序/堆排序.py
+++ 排序/堆排序.py
@@ -9,6 +9,7 @@ def heapify(arr, i):
     largest = i
     if left < arrLen and arr[left] > arr[largest]:
         largest = left
+    
     if right < arrLen and arr[right] > arr[largest]:
         largest = right
 
@@ -25,11 +26,14 @@ def heapSort(arr):
     global arrLen
     arrLen = len(arr)
     buildMaxHeap(arr)
+    
     for i in range(len(arr) - 1, 0, -1):
         swap(arr, 0, i)
         arrLen -= 1
         heapify(arr, 0)
+    
     return arr
+
 
 if __name__ == '__main__':
     print(heapSort([5, 6, 1, 8, 4, 0]))
